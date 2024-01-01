faed7b99131952ac89ed97bca6436688592732aa
Sajib Hira <115892558+Mr-sajibhira@users.noreply.github.com>
2024-01-01 17:44:42 +0600

    Update index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+
+add_filter('add_to_cart_redirect', 'lw_add_to_cart_redirect');
+function lw_add_to_cart_redirect() {
+ global $woocommerce;
+ $lw_redirect_checkout = $woocommerce->cart->get_checkout_url();
+ return $lw_redirect_checkout;
+}
+
+Add this code to change add to cart button text:
+
+add_filter( 'woocommerce_product_single_add_to_cart_text', 'lw_cart_btn_text' );
+add_filter( 'woocommerce_product_add_to_cart_text', 'lw_cart_btn_text' );
+function lw_cart_btn_text() {
+ return __( 'Buy Now!', 'woocommerce' );
+}
